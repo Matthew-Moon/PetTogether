affe0ee8983b92a388c05656039b0a55a1bac326
Matthew <vivainno.matthew@gmail.com>
2025-01-26 02:31:33 +0900

    feat: MyApplication 추가 및 Timber 초기화

---
+++ app/src/main/java/com/matthew/pettogether/MyApplication.kt
@@ -0,0 +1,24 @@
+package com.matthew.pettogether
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.*
+
+
+@HiltAndroidApp
+open class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+
+}
