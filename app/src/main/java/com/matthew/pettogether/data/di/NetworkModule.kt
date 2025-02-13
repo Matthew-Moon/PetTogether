08c6f5b8fdb2dc34798f40832a977413dd97d02c
Matthew <vivainno.matthew@gmail.com>
2025-02-14 02:39:01 +0900

    chore: 위치 변경

---
+++ app/src/main/java/com/matthew/pettogether/data/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.matthew.pettogether.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    }
+
+    companion object {
+        private const val BASE_URL = "https://apis.data.go.kr/B551011/KorPetTourService/"
+    }
+}
