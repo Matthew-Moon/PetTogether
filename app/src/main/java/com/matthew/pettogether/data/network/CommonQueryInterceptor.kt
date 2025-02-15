b41b7efc6df0c2f129b71a65d072607270014c13
Matthew <vivainno.matthew@gmail.com>
2025-02-15 18:21:53 +0900

    feat: Retrofit 인터셉터 추가 - 로깅 인터셉터 - 공통 파라미터 인터셉터

---
+++ app/src/main/java/com/matthew/pettogether/data/network/CommonQueryInterceptor.kt
@@ -0,0 +1,30 @@
+package com.matthew.pettogether.data.network
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class CommonQueryInterceptor : Interceptor {
+    companion object {
+        private const val SERVICE_KEY = "ewS5WWLdbJMGS5s/76P6jXu1rqZBO/+IUAGkuVuMc8FVvDevts1hRC26ED0WivHm6jcRLXgIVcJ1WLLyjC2j/w=="
+        private const val MOBILE_OS = "AND"
+        private const val MOBILE_APP = "PetTogether"
+        private const val TYPE = "json"
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val original = chain.request()
+        val originalHttpUrl = original.url()
+
+        val url = originalHttpUrl.newBuilder()
+            .addQueryParameter("serviceKey", SERVICE_KEY)
+            .addQueryParameter("MobileOS", MOBILE_OS)
+            .addQueryParameter("MobileApp", MOBILE_APP)
+            .addQueryParameter("_type", TYPE)
+            .build()
+
+        val requestBuilder = original.newBuilder()
+            .url(url)
+
+        return chain.proceed(requestBuilder.build())
+    }
+}
